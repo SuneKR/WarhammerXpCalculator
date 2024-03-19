10ee8673147139dcdfdc6bcec81179a148ab3e31
SuneKR <SuneKRmail@gmail.com>
2024-03-19 18:50:57 +0100

    Add a property to ExperienceLog

--- WarhammerXpCalculator/WarhammerXpCalculator/Models/ExperienceLog.cs
+++ WarhammerXpCalculator/WarhammerXpCalculator/Models/ExperienceLog.cs
@@ -4,9 +4,10 @@
     {
         public int Id { get; set; }
         public int CharacterId { get; set; }
+        public DateTime Timestamp { get; set; }
         public int XpSpent { get; set; }
         public bool InCareerDiscount { get; set; }
-        public DateTime Timestamp { get; set; }
+        public string XpBought { get; set; }
         public virtual Character character { get; set; }
     }
 }
